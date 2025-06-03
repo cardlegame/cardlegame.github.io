00a4765e31575defd112105a6bcdfd5bb69ce8cc
SteamDivingBellBot <steam-diving-bell@noreply.github.com>
2025-06-03 00:21:07 -0700

    Fixes

--- .github/workflows/update_update_cards.py
+++ .github/workflows/update_update_cards.py
@@ -16,7 +16,7 @@ def download_set_image(card_set_id):
   expansion_imgs = soup.find_all('img', {'class': 'CardSetLogo'})
   for i, img in enumerate(expansion_imgs[:3]):
     r = requests.get(img['src'])
-    
+
     already_downloaded = False
     for file in Path(f'{repo_root}/public/sets/').glob('*.png'):
       with file.open('rb') as f:
@@ -37,22 +37,14 @@ def add_card_set(new_set_line, new_standard_range):
     contents = f.read().split('\n')
 
   end_line = contents.index('} # end CARD_SETS')
-  contents = contents[:end_line] + new_set_line + contents[end_line:]
-  
+  contents = contents[:end_line] + [new_set_line] + contents[end_line:]
+
   for i in range(len(contents)):
     if contents[i].startswith('  STANDARD_RANGE ='):
       contents[i] = new_standard_range
 
   with update_cards.open('w') as f:
     f.write('\n'.join(contents))
-
-"""
-
-  raise ValueError("Could not find set icon for core set, please rename the image when adjusting the standard range")
-    raise ValueError(f'Found card data from an unknown set: {card.card_set.name}. Was there an expansion recently? If so, please update the CARD_SETS and STANDARD_RANGE variables.')
-
-def 
-"""
 
 if __name__ == '__main__':
   cardid_db, _ = cardxml.load()
@@ -76,4 +68,3 @@ if __name__ == '__main__':
     new_set_line = f"  '{card_set}': {card_set_id:0.1f}, # {card_set_title}"
     new_standard_line = f"  STANDARD_RANGE = [32.0, {card_set_id:0.1f}] # Inclusive on both ends"
     add_card_set(new_set_line, new_standard_line)
-    
